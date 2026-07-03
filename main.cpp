6f1a586204e37208cee1bd1201242d7224ce8f94
loadingSy <abdulrahmanalbredi@gmail.com>
2026-07-03 11:00:56 +0300

    added: save/load, stats menu and resetting

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <cmath>
 #include <cstddef>
+#include <fstream>
 #include <iostream>
 #include <stdexcept>
 #include <string>
@@ -8,16 +9,29 @@
 
 using namespace std;
 
+struct saveData
+{
+    int gameLoopsPlayed = 0;
+    int failedGuessAttempts = 0;
+    int successfullGuessAttempts = 0;
+    int timesWon = 0;
+    int timesLost = 0;
+    int dumbInputs = 0;
+    int points = 0;
+    int pointsGainMultiplier = 1;
+    int pointsDiscountMultiplier = 1;
+    int chosenAttempts = 10;
+    int guessRange = 30;
+};
+
+saveData tempRunningSaveData;
+
 float basePenaltyValue = 5;
 int minGuessRange = 10;
-int guessRange = 30;
-int points = 0;
-int pointsGainMultiplier = 1;
-int pointsDiscountMultiplier = 1;
 int attempts = 10;
-int chosenAttempts = 10;
 int lastDistance = -1;
 bool gamePlayInProggress = true;
+string fileName = "saveFile.txt";
 
 void returnError(string errorMessage);
 int validateInput(string inputToCheck);
@@ -25,14 +39,33 @@ void gameLoop();
 void triggerDefeat(int rightNumber, int userInput);
 int randomInRange(int min, int max);
 int validateToString(string stringToCheck);
-void endGame(bool victory);
+void endGame(bool victory, int rightNumber);
 void mainMenu();
 void mainMenuPanelTriggers(int stateId);
 void settingsMenu();
+void statsMenu();
+bool loadSaveData(const string& fileName, saveData& data);
+bool saveTempData(const string& fileName, const saveData& data);
 
 
 int main()
 {
+    
+    if (loadSaveData(fileName, tempRunningSaveData)) 
+    {
+        cout << "Save data file loaded Successfully!\n";
+    }
+    else {
+        cout << "Save data file Not Found Creating new one...\n";
+        if(saveTempData(fileName, tempRunningSaveData))
+        {
+            cout << "Finished Saving successfully\n";
+        }
+        else {
+            cout << "Saving Failed..\n";
+        }
+        
+    }
     cout << "Press Enter to start: ";
     cin.get();
     mainMenu();
@@ -41,13 +74,13 @@ int main()
 void gameLoop()
 {
     lastDistance = -1;
-    int currentAttempts = chosenAttempts;
+    int currentAttempts = tempRunningSaveData.chosenAttempts;
     cout << "Starting Game with:\n"
-    << chosenAttempts << " attempts\n"
-    << "0-" << guessRange << " range\n";
+    << tempRunningSaveData.chosenAttempts << " attempts\n"
+    << "0-" << tempRunningSaveData.guessRange << " range\n";
     bool continuePlaying = true;
     bool answeredCorrectly = false;
-    int generatedNumber = randomInRange(11, guessRange);
+    int generatedNumber = randomInRange(0, tempRunningSaveData.guessRange);
     while (gamePlayInProggress && continuePlaying) 
     {
         if(!continuePlaying)
@@ -68,9 +101,11 @@ void gameLoop()
         
         
         answeredCorrectly = true;
+        tempRunningSaveData.successfullGuessAttempts++;
     }
 
-    endGame(answeredCorrectly);
+
+    endGame(answeredCorrectly, generatedNumber);
 }
 
 void mainMenu()
@@ -104,8 +139,13 @@ void mainMenuPanelTriggers(int stateId)
         settingsMenu();
         break;
 
+        case 3:
+        statsMenu();
+        break;
+
         default:
         mainMenu();
+        break;
     }
 }
 
@@ -132,7 +172,7 @@ int validateInput(string inputToCheck)
     try
     {
         int processedInput =  stoi(inputToCheck);
-        if(processedInput > guessRange or processedInput < 0)
+        if(processedInput > tempRunningSaveData.guessRange or processedInput < 0)
         {
             return -1;
         }
@@ -171,7 +211,7 @@ void settingsMenu()
             switch (validatedInput) 
             {
                 case 1:
-                    cout << "\nEnter Attempts Amount (max " << attempts << ", current "<< chosenAttempts << "): ";
+                    cout << "\nEnter Attempts Amount (max " << attempts << ", current "<< tempRunningSaveData.chosenAttempts << "): ";
                     playerInput = "";
                     cin >> playerInput;
                     validatedInput = validateToString(playerInput);
@@ -187,13 +227,13 @@ void settingsMenu()
                     }
 
                         cout << "\nSet to: " << validatedInput << " Successfully!\n";
-                    chosenAttempts = validatedInput;
+                    tempRunningSaveData.chosenAttempts = validatedInput;
                     return;
 
                     break;
 
                 case 2:
-                    cout << "\nEnter Range 0-N, (current " <<  guessRange << ", min " << minGuessRange <<"): ";
+                    cout << "\nEnter Range 0-N, (current " <<  tempRunningSaveData.guessRange << ", min " << minGuessRange <<"): ";
                     playerInput = "";
                     cin >> playerInput;
                     validatedInput = validateToString(playerInput);
@@ -209,6 +249,7 @@ void settingsMenu()
                     }
 
                     cout << "\nSet to: " << validatedInput << " Successfully!\n";
+                    tempRunningSaveData.guessRange = validatedInput;
                     return;
                     break;
             }
@@ -221,19 +262,58 @@ void settingsMenu()
     cout << "Input correct";
 }
 
+void statsMenu()
+{
+    // adding save using I/O maybe a bit later
+    cout << "\nStats\n";
+    cout << "Game Loops Played: " << tempRunningSaveData.gameLoopsPlayed << "\n" <<
+    "Failed Guesses: " << tempRunningSaveData.failedGuessAttempts << "\n" <<
+    "Successfull Guesses: " << tempRunningSaveData.successfullGuessAttempts << "\n" <<
+    "Times Won: " << tempRunningSaveData.timesWon << "\n" <<
+    "Times Lost: " << tempRunningSaveData.timesLost << "\n" <<
+    "Dumb inputs: " << tempRunningSaveData.dumbInputs << "\n" <<
+    "Points: " << tempRunningSaveData.points << "\n" <<
+    "Points Gain Multiplier: " << tempRunningSaveData.pointsGainMultiplier << "\n" <<
+    "Points Discount Multiplier: " << tempRunningSaveData.pointsDiscountMultiplier << "\n" <<
+    "Chosen Attempts: " << tempRunningSaveData.chosenAttempts << "\n" <<
+    "Guess Range: 0-" << tempRunningSaveData.guessRange << "\n" <<
+    "\n5. To reset: ";
+    string playerInput;
+    cin >> playerInput;
+
+    int validatedInput = validateToString(playerInput);
+
+    if(validatedInput == 5)
+    {
+        cout << "\nAre you sure you want to reset your data?\nthis action cant be undone\nType (fffddd) to reset... \n ";
+        cin >> playerInput;
+        if(playerInput == "fffddd")
+        {
+            ofstream file(fileName);
+            file.trunc;
+            file.close();
+            loadSaveData(fileName, tempRunningSaveData);
+            cout << "\n Reset Succesfully!";
+        }
+    }
+    cout << "\nPress Enter to return to menu ";
+}
+
 void triggerDefeat(int rightNumber, int userInput)
 {
+    tempRunningSaveData.failedGuessAttempts++;
     string messageOutput;
     string typeOutput = "";
     if(userInput == -1)
     {
         messageOutput = "Your input is Invalid\nPress enter to continue... ";
+        tempRunningSaveData.dumbInputs++;
         cin.get();
     }
     else
     {
         int distance = abs(userInput - rightNumber);
-        int percentageDifference = (distance * 100) / guessRange;
+        int percentageDifference = (distance * 100) / tempRunningSaveData.guessRange;
         
         
 
@@ -282,20 +362,20 @@ void triggerDefeat(int rightNumber, int userInput)
             }
             else if (percentageDifference <= 10)
             {
-                messageOutput = "You are Burning but ";
+                messageOutput = "You are Very Close, but getting ";
             }
             else if (differenceInGuess <= 10) 
             {
-            messageOutput = "You are only a bit ";
+            messageOutput = "You are only getting a bit ";
             } else if (differenceInGuess <= 25) 
             {
-                messageOutput = "You are ";
+                messageOutput = "You are getting ";
             } else if (differenceInGuess <= 50) 
             {
-                messageOutput = "You are Very ";
+                messageOutput = "You are getting Very ";
             } else 
             {
-                messageOutput = "You are Extremely ";
+                messageOutput = "You are getting Extremely ";
             }
             
         }
@@ -305,23 +385,27 @@ void triggerDefeat(int rightNumber, int userInput)
     
 
     cout << messageOutput << typeOutput <<"\n";
-    cin.get();
+    
 }
 
-void endGame(bool victory)
+void endGame(bool victory, int rightAnswer)
 {
+    tempRunningSaveData.gameLoopsPlayed++;
     float pointsToChange = 0.0f;
     if(victory)
     {
-        pointsToChange = ceil((guessRange * 0.2f - min(min(6.0f,chosenAttempts*0.1f),guessRange*0.2f)));
+        tempRunningSaveData.timesWon++;
+        pointsToChange = ceil((tempRunningSaveData.guessRange * 0.2f - min(min(6.0f,tempRunningSaveData.chosenAttempts*0.1f),tempRunningSaveData.guessRange*0.2f)));
         cout << "You won!, Rewarding \"" << pointsToChange << "\" Points\n";
     }
     else {
-        pointsToChange = ceil(-((float)chosenAttempts/guessRange*basePenaltyValue));
-        cout << "You Lost, Charging \"" << pointsToChange << "\"Points\n" ;
+        tempRunningSaveData.timesLost++;
+        pointsToChange = ceil(-((float)tempRunningSaveData.chosenAttempts/tempRunningSaveData.guessRange*basePenaltyValue));
+        cout << "You Lost it was " << rightAnswer << ", Charging \"" << pointsToChange << "\" Points\n" ;
     }
 
-    points += pointsToChange;
+    tempRunningSaveData.points += pointsToChange;
+    saveTempData(fileName, tempRunningSaveData);
 }
 
 int randomInRange(int min, int max)
@@ -330,4 +414,49 @@ int randomInRange(int min, int max)
     mt19937 rng(dev());
     std::uniform_int_distribution<std::mt19937::result_type> dist6(min,max);
     return dist6(rng);
+}
+
+bool loadSaveData(const string& fileName, saveData& data)
+{
+    ifstream file(fileName);
+
+    if(!file.is_open()) return false;
+
+    file >> data.gameLoopsPlayed;
+    file >> data.failedGuessAttempts;
+    file >> data.successfullGuessAttempts;
+    file >> data.timesWon;
+    file >> data.timesLost;
+    file >> data.dumbInputs;
+    file >> data.points;
+    file >> data.pointsGainMultiplier;
+    file >> data.pointsDiscountMultiplier;
+    file >> data.chosenAttempts;
+    file >> data.guessRange;
+    
+
+    file.close();
+    return true;
+}
+
+bool saveTempData(const string& fileName, const saveData& data)
+{
+    ofstream file(fileName);
+
+    if(!file.is_open()) return false;
+
+    file << data.gameLoopsPlayed << "\n";
+    file << data.failedGuessAttempts << "\n";
+    file << data.successfullGuessAttempts << "\n";
+    file << data.timesWon << "\n";
+    file << data.timesLost << "\n";
+    file << data.dumbInputs << "\n";
+    file << data.points << "\n";
+    file << data.pointsGainMultiplier << "\n";
+    file << data.pointsDiscountMultiplier << "\n";
+    file << data.chosenAttempts << "\n";
+    file << data.guessRange << "\n";
+    
+    file.close();
+    return true;
 }
